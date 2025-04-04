b071586fe16821cf1f51ca86267290b37a40eafb
Vinicius Macedo <viniciusmcode@gmail.com>
2025-04-04 19:18:32 -0300

    feat: include main div and main text

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,17 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <div className="main-text">
+          <span className="tic-toe">tic.</span>
+          <span className="tac">tac.</span>
+          <span className="tic-toe">toe.</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
