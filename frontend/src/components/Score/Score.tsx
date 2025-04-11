83f487570a03696f62eb4255f1e7400ac7eeaf19
Vinicius Botelho <viniciusmcode@gmail.com>
2025-04-11 19:16:44 -0300

    status removed

--- frontend/src/components/Score/Score.tsx
+++ frontend/src/components/Score/Score.tsx
@@ -1,12 +1,18 @@
 import styles from "./Score.module.css";
 
-const Score: React.FC<{ player: string; score: number }> = ({ player, score }) => {
-    return (
-        <div className={styles.score}>
-            <span className={styles.textScore}>{player}</span>
-            <span className={`${styles.textScore} ${styles.textScoreNumber}`}>{score}</span>
-        </div>
-    );
+const Score: React.FC<{ player: string; score: number, backgroundColor: string}> = ({
+  player,
+  score,
+  backgroundColor
+}) => {
+  return (
+    <div className={styles.score} style={{backgroundColor}}>
+      <span className={styles.textScore}>{player}</span>
+      <span className={`${styles.textScore} ${styles.textScoreNumber}`}>
+        {score}
+      </span>
+    </div>
+  );
 };
 
 export default Score;
